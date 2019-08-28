ae24d86da475aba59e8e4ef56cbd1a429a3f2a4a
Thor Sideburns <sideboard@google.com>
2019-08-28 09:26:34 -0700

    Fixing tail function

--- builtins.cpp
+++ builtins.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <sstream>
+#include <iostream>
+#include "evaluator.hpp"
 
 namespace builtin
 {
@@ -77,18 +80,17 @@ std::unordered_map<std::string, std::shared_ptr<object::BuiltIn>> built_ins = {
              }
 
              int len_elems = array_obj->elements_.size();
-             if (len_elems > 1)
+             if (len_elems > 0)
              {
-                 auto return_array = std::make_shared<object::Array>(
-                     std::vector<std::shared_ptr<object::Object>>());
+             auto return_array = std::make_shared<object::Array>(
+                 std::vector<std::shared_ptr<object::Object>>());
 
                  for (int i = 1; i < len_elems; i++)
                      return_array->elements_.push_back(array_obj->elements_[i]);
-
-                 return return_array;
+                return return_array;
              }
-
              return evaluator::NULLL;
+
          })},
     {"last",
      std::make_shared<object::BuiltIn>(
@@ -143,6 +145,20 @@ std::unordered_map<std::string, std::shared_ptr<object::BuiltIn>> built_ins = {
 
              return return_array;
          })},
+    {"puts",
+     std::make_shared<object::BuiltIn>(
+         [](std::vector<std::shared_ptr<object::Object>> args)
+             -> std::shared_ptr<object::Object> {
+
+               std::stringstream out;
+               for (auto& o : args) {
+                 out << o->Inspect();
+               }
+
+               std::cout << out.str() << std::endl;
+
+               return evaluator::NULLL;
+             })},
 };
 
 } // namespace builtin
